3521979c71c744f95a1b1c035d97f3ef9d0807d6
icyllite <icyllite@gmail.com>
2025-03-06 08:44:47 +0000

    port dxvk 1.4.2

--- src/dxvk/dxvk_extensions.h
+++ src/dxvk/dxvk_extensions.h
@@ -277,7 +277,7 @@ namespace dxvk {
     DxvkExt khrDrawIndirectCount              = { VK_KHR_DRAW_INDIRECT_COUNT_EXTENSION_NAME,                DxvkExtMode::Optional };
     DxvkExt khrDriverProperties               = { VK_KHR_DRIVER_PROPERTIES_EXTENSION_NAME,                  DxvkExtMode::Optional };
     DxvkExt khrGetMemoryRequirements2         = { VK_KHR_GET_MEMORY_REQUIREMENTS_2_EXTENSION_NAME,          DxvkExtMode::Required };
-    DxvkExt khrImageFormatList                = { VK_KHR_IMAGE_FORMAT_LIST_EXTENSION_NAME,                  DxvkExtMode::Required };
+    DxvkExt khrImageFormatList                = { VK_KHR_IMAGE_FORMAT_LIST_EXTENSION_NAME,                  DxvkExtMode::Optional };
     DxvkExt khrMaintenance1                   = { VK_KHR_MAINTENANCE1_EXTENSION_NAME,                       DxvkExtMode::Required };
     DxvkExt khrMaintenance2                   = { VK_KHR_MAINTENANCE2_EXTENSION_NAME,                       DxvkExtMode::Required };
     DxvkExt khrSamplerMirrorClampToEdge       = { VK_KHR_SAMPLER_MIRROR_CLAMP_TO_EDGE_EXTENSION_NAME,       DxvkExtMode::Optional };
